90f0b59bdb51650e4a5104b41de9bd7c7aa89ed1
maryhug <maryhug.viviana@gmail.com>
2024-06-02 21:09:05 -0500

    Nueva Mod

--- src/Panel/JDoctoresMenu.java
+++ src/Panel/JDoctoresMenu.java
@@ -19,7 +19,8 @@ import javax.swing.border.LineBorder;
 public class JDoctoresMenu extends JPanel {
 
     private static final long serialVersionUID = 1L;
-    private String nombreUsuario; // Campo para almacenar el nombre del usuario
+    @SuppressWarnings("unused")
+	private String nombreUsuario; // Campo para almacenar el nombre del usuario
 
     public JDoctoresMenu(String nombreUsuario) {
         this.nombreUsuario = nombreUsuario; // Asignar el nombre del usuario al campo
@@ -30,7 +31,7 @@ public class JDoctoresMenu extends JPanel {
             public void actionPerformed(ActionEvent e) {
                 setVisible(false);
                 // Pasar el nombre del usuario como ID del doctor al constructor de JIngresarPacientes
-                JIngresarPacientes ingresarPacientesPanel = new JIngresarPacientes(nombreUsuario);
+                JPacientes ingresarPacientesPanel = new JPacientes(nombreUsuario);
                 JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(JDoctoresMenu.this);
                 frame.setContentPane(ingresarPacientesPanel);
                 frame.setVisible(true);
